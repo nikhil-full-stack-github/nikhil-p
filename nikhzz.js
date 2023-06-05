0e4f60512b6258b98da809d8dcda9aef49895dfb
unknown <nikhilppry2001@gmail.com>
2023-06-05 21:19:19 +0530

    first commit

---
+++ nikhzz.js
@@ -0,0 +1,17 @@
+let a=51
+
+if(a>90 && a<=100){
+console.log("grade A")
+}
+else if(a>80 && a<=90){
+    console.log("grade B")
+}
+else if(a>70 && a<=80){
+    console.log("grade C")
+}
+else if(a>60 && a<=70){
+    console.log("grade D")
+}
+else if(a>50 && a<=60){
+    console.log("failde")
+}
